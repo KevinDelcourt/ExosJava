b0d4c249cabf8355ca1b2fbacdec496599f0652d
dlk1560a <dlk1560a@r-info-rubi-e02.iut.iut-tlse3.fr>
2018-09-25 11:25:19 +0200

    test

--- BatailleNavale/src/primitiveLanguage/Bateau.java
+++ BatailleNavale/src/primitiveLanguage/Bateau.java
@@ -1,18 +1,32 @@
 package primitiveLanguage;
 
+/**Un bateau, avec un nom, une taille, un etat 
+ * 
+ * @author dlk1560a
+ *
+ */
 public class Bateau {
 
-	protected String nom;
-	protected int tailleBateau;
-	protected char etatBateau = 'S';
-	protected Element[] bateau;
+	private String nom;
+	private int tailleBateau;
+	private char etatBateau = 'S';
+	private Element[] bateau;
 	
-	public Bateau(String nom, int tailleBateau) {
+	/**Constructeur
+	 * @param nom nom du bateau
+	 * @param tailleBateau taille du bateau
+	 */
+	Bateau(String nom, int tailleBateau) {
 		this.nom = nom;
 		this.tailleBateau = tailleBateau;
 		this.bateau = new Element[tailleBateau];
 	}
 	
+	/**Place tout les éléments du bateau selon les coordonnées et le sens
+	 * @param lettre coordonnée 1
+	 * @param chiffre coordonnée 2
+	 * @param sens V ou H, vertical ou horizontal
+	 */
 	public void placerBateau(char lettre, int chiffre, char sens) {
 		
 		if(sens == 'H') {
--- BatailleNavale/src/primitiveLanguage/Element.java
+++ BatailleNavale/src/primitiveLanguage/Element.java
@@ -1,7 +1,7 @@
 package primitiveLanguage;
 
 /**
- * Un bateau
+ * Un élément d'un bateau, occupe une case
  * 
  * @author dlk1560a
  *
@@ -23,21 +23,33 @@ public class Element {
 			this.chiffre = chiffre;
 		}
 		
-		//Le bateau est-il touché
+		
+		/**vrai si l'élément est touché, faux sinon
+		 * @return elementtouche
+		 */
 		public boolean isElementTouche() {
 			
 			return elementTouche;
 			
 		}
 		
-		//Retourne vrai si les coordonnées en paramètre correspondent à celles du bateau
+		
+		/** vrai si les coordonnées en paramètre correspondent à celles du bateau
+		 * @param lettre coordonnée 1
+		 * @param chiffre coordonnée 2
+		 * @param sens H ou V, donne le sens
+		 * @return vrai ou faux
+		 */
 		public boolean verifierCoordonnees(char lettre, int chiffre, char sens) {
 			
 			return (this.lettre == lettre)&&(this.chiffre == chiffre);
 			
 		}
 		
-		//Le bateau est touché
+		
+		/**
+		 * L'élément est touché, passe elementTouche à true
+		 */
 		public void touche() {
 			
 			this.elementTouche = true;
