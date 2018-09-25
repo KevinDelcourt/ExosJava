3440595ba35b1b959091de331603e24c5890feb3
dlk1560a <dlk1560a@r-info-rubi-e02.iut.iut-tlse3.fr>
2018-09-25 10:09:32 +0200

    test

--- KataTen/KataTennis/src/TestTennisGame.java
+++ KataTen/KataTennis/src/TestTennisGame.java
@@ -24,5 +24,79 @@ public class TestTennisGame {
 		assertEquals("love - love" , this.game.currentScore());
 		
 	}
+	
+	@Test
+	public void testJoueur1Marque1Point() {
+		
+		this.game.player1Scores();
+		assertEquals("fifteen - love" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur2Marque1Point() {
+		
+		this.game.player2Scores();
+		assertEquals("love - fifteen" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur1Et2Marquent1Point() {
+		
+		this.game.player2Scores();
+		this.game.player1Scores();
+		assertEquals("fifteen - fifteen" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur1Marque2Points() {
+		
+		this.game.player1Scores();
+		this.game.player1Scores();
+		assertEquals("thirty - love" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur2Marque2Points() {
+		
+		this.game.player2Scores();
+		this.game.player2Scores();
+		assertEquals("love - thirty" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur1Et2Marquent2Points() {
+		
+		this.game.player2Scores();
+		this.game.player2Scores();
+		this.game.player1Scores();
+		this.game.player1Scores();
+		assertEquals("thirty - thirty" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur1Marque3Points() {
+		
+		this.game.player1Scores();
+		this.game.player1Scores();
+		this.game.player1Scores();
+		assertEquals("forty - love" , this.game.currentScore());
+		
+	}
+	
+	@Test
+	public void testJoueur2Marque3Points() {
+		
+		this.game.player2Scores();
+		this.game.player2Scores();
+		this.game.player2Scores();
+		assertEquals("love - forty" , this.game.currentScore());
+		
+	}
 
 }
